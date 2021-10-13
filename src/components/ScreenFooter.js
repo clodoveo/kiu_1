82a35f31708f3ded2132b092ec481ff7ff95dc02
Carlo Veneziano <carlorfeo@gmail.com>
2021-10-14 00:27:26 +0200

    info screen

--- src/components/ScreenFooter.js
+++ src/components/ScreenFooter.js
@@ -1,47 +1,6 @@
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
-export default styled(({ text, className }) => {
-  return (
-    <div className={className}>
-      {links.map((item, index) => (
-        <FooterLink key={index} {...item} />
-      ))}
-    </div>
-  );
-})`
-  display: flex;
-  padding: 10px 0;
-`;
-
-const FooterLink = styled(({ className, label, iconClass, linkTo }) => (
-  <Link to={linkTo} className={className}>
-    <span className="icon">
-      <i className={iconClass} />
-    </span>
-    <span className="label">{label}</span>
-  </Link>
-))`
-  text-align: center;
-  width: 25%;
-  cursor: pointer;
-  text-decoration: none;
-
-  & + & {
-    border-left: 1px solid #ccc;
-  }
-
-  .icon {
-    color: #265a32;
-    display: block;
-    font-size: 27px;
-  }
-  .label {
-    font-size: 14px;
-    color: #607464;
-  }
-`;
-
 const links = [
   {
     label: "Raggiungi",
@@ -64,3 +23,45 @@ const links = [
     linkTo: "/chat"
   }
 ];
+
+export default function ScreenFooter() {
+  return (
+    <Styled>
+      {links.map(({ linkTo, iconClass, label }, index) => (
+        <Link key={index} to={linkTo}>
+          <span className="icon">
+            <i className={iconClass} />
+          </span>
+          <span className="label">{label}</span>
+        </Link>
+      ))}
+    </Styled>
+  );
+}
+
+const Styled = styled.div`
+  display: flex;
+  padding: 10px 0;
+  background: #fff;
+  margin-top: 1em;
+
+  a {
+    text-align: center;
+    width: 25%;
+    text-decoration: none;
+  }
+
+  a + a {
+    border-left: 1px solid #ccc;
+  }
+
+  .icon {
+    color: #265a32;
+    display: block;
+    font-size: 27px;
+  }
+  .label {
+    font-size: 14px;
+    color: #607464;
+  }
+`;
