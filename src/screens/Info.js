1a5fd096dcca89c77c261ba0946ee394906354a6
Carlo Veneziano <carlorfeo@gmail.com>
2021-10-20 17:10:37 +0200

    info card su componente esterno

--- src/screens/Info.js
+++ src/screens/Info.js
@@ -5,90 +5,32 @@ import AnimatedFrame from "../components/AnimatedFrame";
 import ScreenHeader from "../components/ScreenHeader";
 import ScreenFooter from "../components/ScreenFooter";
 import LoadingSpinner from "../components/LoadingSpinner";
+import InfoCards from "../components/InfoCards";
 
-import info from "../api/info";
+import infoApi from "../api/info";
 
 export default function InfoScreen() {
-  const [infoData, setInfoData] = useState(false);
+  const [items, setItems] = useState(null);
 
-  if (!infoData) {
+  if (! items) {
     const langId = 1;
 
-    info.fetch(langId).then(setInfoData);
+    infoApi.fetch(langId).then(setItems);
+
+    return <LoadingSpinner/>
   }
+
+  const body = items ?
+    <InfoCards items={items} /> :
+    <LoadingSpinner />
 
   return (
     <AnimatedFrame scrollable>
-      <Styled>
+      <div>
         <ScreenHeader text="Informazioni utili" />
-
-        <div className="cards">
-          {infoData === false && <LoadingSpinner />}
-
-          {infoData &&
-            infoData.map((data, index) => (
-              <div key={index} className="card">
-                {data.imgUrl && (
-                  <div className="img">
-                    <img src={data.imgUrl} alt={data.title} />
-                  </div>
-                )}
-
-                <div className="content">
-                  <div className="title">{data.title}</div>
-                  <div dangerouslySetInnerHTML={{ __html: data.text }} />
-                </div>
-              </div>
-            ))}
-        </div>
-
+        {body}
         <ScreenFooter />
-      </Styled>
+      </div>
     </AnimatedFrame>
   );
 }
-
-const Styled = styled.div`
-  .cards {
-    border-top: 1px solid #eee;
-    padding: 1em;
-
-    .card + .card {
-      margin-top: 1.8em;
-    }
-    .card {
-      padding: 1em;
-      box-shadow: 0px 0px 42px rgba(0, 0, 0, 0.1);
-      border-radius: 10px;
-
-      img {
-        max-width: 100%;
-      }
-
-      .content {
-        padding: 10px;
-
-        .title {
-          font-size: 23px;
-          line-height: 36px;
-          color: #265a32;
-          font-weight: bold;
-        }
-
-        p {
-          margin: 6px 0;
-        }
-
-        .links {
-          margin-top: 1.2em;
-          text-align: right;
-          a {
-            font-size: 41px;
-            color: #265a32;
-            margin: 10px;
-          }
-        }
-      }
-    }
-  }
-`;
