8aa61aa7907f2c6e1805dbd568213ccf2b76ce3b
clodoveo <filippo@ramointeractive.com>
2021-10-21 12:32:20 +0200

    manifest + opener wa

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -7,8 +7,9 @@ export default function () {
     title: "Chatta con Giuseppe",
     text: "Verrai reindirizzato su Whatsapp",
     link: {
-      to: "/menu",
-      label: "Chatta ora"
+      to: "https://wa.me/+393929278281",
+      label: "Chatta ora",
+      external:true
     },
     image: "https://giomiapp.terotero.it/img/original/app/agente1.png",
     icon:"https://giomiapp.terotero.it/img/original/app/chat-icon.png"
