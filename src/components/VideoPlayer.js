d5a2585a4a5f5476c9cc7089cf60f0f04ed53c11
Carlo Veneziano <carlorfeo@gmail.com>
2022-02-28 11:42:53 +0100

    video jump controls

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import styled from "styled-components";
 
 import { AdvancedVideo, AdvancedImage } from "@cloudinary/react";
 import {
+  Cloudinary,
   CloudinaryVideo,
   CloudinaryImage,
   Transformation,
@@ -15,11 +16,13 @@ import { volume } from "@cloudinary/url-gen/actions/videoEdit";
 import { scale } from "@cloudinary/url-gen/actions/resize";
 
 export default function ({ path, cloudName, mediaWidth, audio, resourcetype }) {
+  const videoRef = useRef();
+
   let media;
+
   switch (resourcetype) {
     case "video":
       media = new CloudinaryVideo(path, { cloudName });
-      media.resize(scale().width(mediaWidth));
 
       if (audio) {
         media.videoEdit(new Transformation().effect(volume("mute")));
@@ -37,21 +40,129 @@ export default function ({ path, cloudName, mediaWidth, audio, resourcetype }) {
       break;
   }
 
+  return (
+    <StyledPlayer>
+      <AdvancedVideo
+        ref={videoRef}
+        cldVid={media}
+        controls
+        onPause={pauseHandler}
+      />
+      <JumpControls getVideoEl={getVideoEl} onJump={jump} />
+    </StyledPlayer>
+  );
+
+  function getVideoEl() {
+    if (!videoRef.current) {
+      return;
+    }
+
+    return videoRef.current.videoRef.current;
+  }
+
+  function pauseHandler(event) {}
+
+  function jump(offset) {
+    const videoEl = getVideoEl();
+
+    if (!videoEl) {
+      return;
+    }
+
+    videoEl.currentTime = getSeekTime(offset, videoEl);
+    videoEl.play();
+  }
+
+  function getSeekTime(offset, videoEl) {
+    if (videoEl.currentTime + offset < 0) {
+      return 0;
+    }
+
+    if (videoEl.currentTime + offset >= videoEl.duration) {
+      return videoEl.duration;
+    }
+
+    return videoEl.currentTime + offset;
+  }
+}
+
+function JumpControls({ onJump, getVideoEl }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const videoEl = getVideoEl();
+    if (!videoEl) {
+      return;
+    }
+
+    videoEl.addEventListener("pause", () => {
+      setIsVisible(true);
+    });
+
+    videoEl.addEventListener("play", () => {
+      setIsVisible(false);
+    });
+  }, []);
+
   const style = {
-    // background: `url("")`
+    display: isVisible ? "block" : "none",
   };
 
   return (
-    <VideoPlayer style={style}>
-      <AdvancedVideo cldVid={media} controls />
-    </VideoPlayer>
+    <div style={style}>
+      <JumpControl offset={-10} direction="left" onClick={jumpHandler} />
+      <JumpControl offset={10} direction="right" onClick={jumpHandler} />
+    </div>
   );
+
+  function jumpHandler(offset) {
+    onJump(offset);
+  }
 }
 
-const VideoPlayer = styled.div`
+function JumpControl({ onClick, offset, direction }) {
+  const className = "jump " + direction;
+
+  return <div className={className} onClick={() => onClick(offset)}></div>;
+}
+
+const StyledPlayer = styled.div`
   width: 100%;
+  position: relative;
 
   video {
     width: 100%;
+  }
+
+  .jump {
+    position: absolute;
+    top: 0;
+    bottom: 80px;
+    // background: #f008;
+    font-family: "Font Awesome 6 free";
+    font-weight: 900;
+
+    :before {
+      position: absolute;
+      left: calc(50% - 0.5em);
+      top: calc(50% - 0.5em);
+      color: #fff;
+    }
+    &.left {
+      left: 0;
+      right: 60%;
+
+      :before {
+        content: "\f04a";
+      }
+    }
+    &.right {
+      left: 60%;
+      right: 0;
+
+      :before {
+        content: "\f04e";
+      }
+    }
   }
 `;
